b567f7b5de780131b0226b539fed9b6eba54d008
tylersemel <tylersemel@gmail.com>
2024-10-10 16:12:09 -0400

    Add function to randomly get rock, paper, or scissors

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hi");
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 100);
+
+    if (randomNum <= Math.floor(100 / 3)) {
+        return "rock";
+    }
+    else if (randomNum <= Math.floor(100 / 3) * 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
